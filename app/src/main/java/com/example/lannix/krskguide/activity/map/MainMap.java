fe9808cf63eb288034266978846d6ae558a937b5
javaAdm <mr.serega20@mail.ru>
2018-11-09 01:24:27 +0700

    Работает!!(yt dct)

--- app/src/main/java/com/example/lannix/krskguide/activity/map/MainMap.java
+++ app/src/main/java/com/example/lannix/krskguide/activity/map/MainMap.java
@@ -3,6 +3,7 @@ package com.example.lannix.krskguide.activity.map;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 import com.example.lannix.krskguide.R;
 import com.example.lannix.krskguide.database.sight_db.Sight;
 import com.example.lannix.krskguide.fragment.map.InfoOfObjectsFragment;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,7 +28,12 @@ import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PointOfInterest;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsResult;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -63,10 +70,14 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback, Mai
         isNoMarker = true;
         mFlag=false;
 
+
+
     }
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
+
+
 
         try {
             // Customise the styling of the base map using a JSON object defined
@@ -84,7 +95,7 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback, Mai
 
         markeR = googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0)));
         markeR.setVisible(true);
-
+        final ArrayList<Sight> dB = DB_SIGHTS.selectAll();
 
         //Слушатель клика по достопримечательностям
         GoogleMap.OnGroundOverlayClickListener mClickListener = new GoogleMap.OnGroundOverlayClickListener() {
@@ -100,6 +111,7 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback, Mai
                 CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(groundOverlay.getPosition());
                 googleMap.animateCamera(cameraUpdate);
                 fragmentMapInfoCreate();
+                //drowLines(googleMap,dB);
             }
         };
         googleMap.setOnGroundOverlayClickListener(mClickListener);
@@ -147,7 +159,7 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback, Mai
         googleMap.setOnMarkerClickListener(onMarkerClickListener);
 
 
-        final ArrayList<Sight> dB = DB_SIGHTS.selectAll();
+
 
 
 
@@ -198,6 +210,7 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback, Mai
             groundOverlay.setTag(dB.get(i).getId());*/
 
         }
+        //drowLines(googleMap,dB);
 
 
         //googleMap.addGroundOverlay(newarkMap);
@@ -273,4 +286,54 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback, Mai
         if (mFlag) markeR.setVisible(true);else markeR.setVisible(false);
 
     }
+
+    void drowLines(GoogleMap googleMap,ArrayList<Sight> dB){
+        GeoApiContext geoApiContext = new GeoApiContext.Builder()
+                .apiKey("AIzaSyBF65lgDKjLRHDxSbuOU7g2-2nb7BPppg4")
+                .build();
+
+//Здесь будет наш итоговый путь состоящий из набора точек
+        DirectionsResult result = null;
+        try {
+            result = DirectionsApi.newRequest(geoApiContext).origin(String.valueOf(new LatLng(dB.get(0).getCoordinates_latitude(), dB.get(0).getCoordinates_longitude())))
+                    .destination(String.valueOf(new LatLng(dB.get(1).getCoordinates_latitude(), dB.get(1).getCoordinates_longitude())))
+                    .await();
+            // .origin()//Место старта
+            //.destination(String.valueOf(places.get(places.size() - 1)))//Пункт назначения
+            //.waypoints(places.get(1), places.get(2)).await();//Промежуточные точки. Да, не очень красиво, можно через цикл, но зато наглядно
+        } catch (InterruptedException | IOException | com.google.maps.errors.ApiException e) {
+            e.printStackTrace();
+        }
+
+//Преобразование итогового пути в набор точек
+        assert result != null;
+        List<com.google.maps.model.LatLng> path = result.routes[0].overviewPolyline.decodePath();
+
+//Линия которую будем рисовать
+        PolylineOptions line = new PolylineOptions();
+
+        LatLngBounds.Builder latLngBuilder = new LatLngBounds.Builder();
+
+//Проходимся по всем точкам, добавляем их в Polyline и в LanLngBounds.Builder
+        for (int i = 0; i < path.size(); i++) {
+            line.add(new com.google.android.gms.maps.model.LatLng(path.get(i).lat, path.get(i).lng));
+            latLngBuilder.include(new com.google.android.gms.maps.model.LatLng(path.get(i).lat, path.get(i).lng));
+        }
+
+//Делаем линию более менее симпатичное
+        line.width(16f).color(R.color.colorPrimary);
+
+//Добавляем линию на карту
+        googleMap.addPolyline(line);
+
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int screenWidth = displaymetrics.widthPixels;
+        int screenHeight = displaymetrics.heightPixels;
+//Выставляем камеру на нужную нам позицию
+        LatLngBounds latLngBounds = latLngBuilder.build();
+        CameraUpdate track = CameraUpdateFactory.newLatLngBounds(latLngBounds, screenWidth,screenHeight , 25);//width это размер нашего экрана
+        googleMap.moveCamera(track);
+    }
+
 }
